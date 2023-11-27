cf851d22f67d0468db9eaa961ab9ef1b83ff0e74
sonicbigboom <sbb-github@dsgt.com>
2023-11-26 20:17:05 -0500

    Other agents.

---
+++ cs534/train-dmc.py
@@ -0,0 +1,47 @@
+
+import os
+import argparse
+from random import randrange
+
+import torch
+
+import rlcard
+from agents.dqn_rule_agent import DQNAgent
+from agents.nfsp_rule_agent import NFSPAgent
+from agents.dmc_rule_trainer import DMCTrainer
+from agents.rule_filter import gin_rummy_rule_filter
+from utils.logger import (
+    Logger,
+    plot_curve
+)
+from rlcard.rlcard.utils import (
+    get_device,
+    set_seed,
+    tournament,
+    reorganize
+)
+from rlcard.rlcard.envs.gin_rummy import GinRummyEnv
+from rlcard.rlcard.models.gin_rummy_rule_models import GinRummyNoviceRuleAgent
+from rlcard.rlcard.agents import RandomAgent
+
+if __name__ == '__main__':
+
+    # Prepare testing.
+    seed = randrange(10000)
+
+    set_seed(seed)
+
+    config = {
+        'allow_step_back': False,
+        'seed': seed,
+    }
+    env = GinRummyEnv(config)
+
+    checkpoint_path = "cs534/models/dmc/checkpoint/"
+
+    agent = DMCTrainer(
+            env=env,
+            savedir=checkpoint_path,
+            rule=gin_rummy_rule_filter
+        )
+    agent.start()
--- cs534/train.py
+++ cs534/train.py
@@ -4,9 +4,9 @@ from random import randrange
 
 import torch
 
-import rlcard
 from agents.dqn_rule_agent import DQNAgent
 from agents.nfsp_rule_agent import NFSPAgent
+from agents.cfr_rule_agent import CFRAgent
 from agents.rule_filter import gin_rummy_rule_filter
 from utils.logger import (
     Logger,
@@ -22,7 +22,7 @@ from rlcard.envs.gin_rummy import GinRummyEnv
 from rlcard.models.gin_rummy_rule_models import GinRummyNoviceRuleAgent
 from rlcard.agents import RandomAgent
 
-available_agents = ["dqn", "nfsp"]
+available_agents = ["dqn", "nfsp", "cfr"]
 available_agents_message = "Potential agents are '" + "', '".join(available_agents) + "'"
 
 def get_agent_name(args):
@@ -88,6 +88,18 @@ def configure_agent_training(agent_name, env, args):
                     save_path=checkpoint_path,
                     rule=gin_rummy_rule_filter
                 )
+    elif agent_name == "cfr":
+        env.allow_step_back = True
+        env.game.allow_step_back = True
+        checkpoint_path = "cs534/models/cfr/checkpoint/"
+        agent = CFRAgent(env, checkpoint_path)
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
+        if os.path.exists(checkpoint_path + "checkpoint_cfr.pt"):
+            CFRAgent.load()
+            agent.rule = gin_rummy_rule_filter
+            with open(checkpoint_path + "checkpoint.txt") as f:
+                start_episodes = int(next(f))
         
     if (args.episodes > 0):
         num_episodes = args.episodes
@@ -148,28 +160,35 @@ if __name__ == '__main__':
     model_dir = f"cs534/models/{agent_name}/"
     checkpoint_path = os.path.join(model_dir, "checkpoint/")
     checkpoint_txt_path = os.path.join(checkpoint_path, "checkpoint.txt")
+    eval_interval = 2000
+    if agent_name == 'cfr':
+        eval_interval = 2
+    save_interval = eval_interval * 5
     with Logger(model_dir + "log/") as logger:
         # Get the paths
         csv_path, fig_path = logger.csv_path, logger.fig_path
 
         for episode in range(start_episodes + 1, num_episodes + start_episodes + 1):
-            if agent_name == 'nfsp':
-                agent.sample_episode_policy()
+            if agent_name == 'cfr':
+                print('\rIteration {}'.format(episode), end='')
+                agent.train()
+            else:
+                if agent_name == 'nfsp':
+                    agent.sample_episode_policy()
 
-            # Generate data from the environment
-            trajectories, payoffs = env.run(is_training=True)
+                # Generate data from the environment
+                trajectories, payoffs = env.run(is_training=True)
 
-            # Reorganaize the data to be state, action, reward, next_state, done
-            trajectories = reorganize(trajectories, payoffs)
+                # Reorganaize the data to be state, action, reward, next_state, done
+                trajectories = reorganize(trajectories, payoffs)
 
-            # Feed transitions into agent memory, and train the agent
-            # Here, we assume that DQN always plays the first position
-            # and the other players play randomly (if any)
-            for ts in trajectories[0]:
-                agent.feed(ts)
+                # Feed transitions into agent memory, and train the agent
+                # Here, we assume that DQN always plays the first position
+                for ts in trajectories[0]:
+                    agent.feed(ts)
 
             # Evaluate the performance. Play with random agents.
-            if episode % 2000 == 0 or episode == 1:
+            if episode % eval_interval == 0 or episode == 1:
                 print("\n*****************************************************")
                 print(f"Running episode {episode} evaluation tournament.")
                 logger.store_performance(
@@ -186,7 +205,7 @@ if __name__ == '__main__':
                 print(f"\nFinished episode {episode} evaluation tournament.")
                 print("*****************************************************\n")
 
-                if episode % 10000 == 0:
+                if episode % save_interval == 0:
                     logger.flush_performance()
                     save_path = os.path.join(model_dir, f"{agent_name}_{episode}.pth")
                     torch.save(agent, save_path)
